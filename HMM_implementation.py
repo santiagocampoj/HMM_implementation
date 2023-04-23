73c0298309f2882f41756fedd7432ad65e9b16c3
Santiago Campo <santiagocampojurado@gmail.com>
2023-04-23 22:12:48 +0200

    Update HMM_implementation.py

--- HMM_implementation.py
+++ HMM_implementation.py
@@ -1,28 +1,3 @@
-"""HMM implementation
-Problem definition:
-
-– A teacher assigns the students two different kinds of homework:
-• Easy exercises that can be solved in 5 minutes
-• Difficult exercises that are solved in 1 hour
-
-– The assignation of homework depends on the mood of the teacher:
-• Good mood: 80% of times an easy work is assigned and 20% a difficult one
-• Neutral mood: 50% of times the assigned work is easy and 50% of times difficult
-• Bad mood: 10% of times the work assigned is easy and 90% of times difficult
-
-– The teacher hides his/her mood, but we know that:
-• If one day he/she is in a good mood, the following day there is a 20% chance the mood does
-not change, a 30% chance that the mood is neutral and 50% chance that it is bad
-• If one day he/she is in a bad mood, the following day there is a 80% chance the mood does
-not change and 20% chance that the mood is neutral
-• Finally, if the mood is neutral there is a 20% chance of continuing with the same mood the
-following day, 20% chance of being in a good mood and 60% chance of being in a bad mood
-
-– The students only know the kind of homework assigned
-– At the beginning of the course the teacher returns from holidays, 80% of
-times in a good mood and 20% of times in neutral mood
-"""
-
 import numpy as np
 
 class HiddenMarkovModel:
